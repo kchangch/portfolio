246b69b3ac0c17edd6d7036ea32c5f7c792125c3
Argenis Chang <kchangch@Argeniss-MacBook-Air.local>
2025-06-04 23:57:13 -0700

    revert styles from work experience

--- src/components/pages/SingleWork.js
+++ src/components/pages/SingleWork.js
@@ -5,6 +5,7 @@ import BlockContent from "@sanity/block-content-to-react";
 import Particles from "react-tsparticles";
 import particlesConfig from "../Particles/particlesConfig";
 import LoadingSpinner from "../Loading/LoadingSpinner";
+import FadeIn from "react-fade-in";
 
 const SingleWork = () => {
 	const [singlePost, setSinglePost] = useState(null);
@@ -75,42 +76,33 @@ const SingleWork = () => {
 	}
 
 	return (
-		<main className="relative md:absolute w-full min-h-screen">
+		<main className="relative md:absolute w-full h-screen overflow-hidden">
 			<Particles
 				params={particlesConfig}
 				className="bg-black absolute object-cover w-full h-full"
 			/>
 			<Suspense fallback={<LoadingSpinner message="Loading content..." />}>
-				<article className="container mx-auto">
-					<header className="relative">
-						<div className="absolute h-full w-full flex items-center justify-center p-8">
-							<div className="bg-white bg-opacity-75 rounded p-12">
-								<h1 className="text-3xl lg:text-6xl mb-4">{singlePost.title}</h1>
-								<div className="flex justify-center text-gray-800">
-									<img
-										src={singlePost.authorImage}
-										alt={singlePost.name}
-										className="w-10 h-10 rounded-full"
-									/>
-									<p className="flex items-center pl-2 text-2xl">{singlePost.name}</p>
-								</div>
-							</div>
+			<article className="relative flex justify-center min-h-screen pt-12 lg:pt-20 px-8">
+				<div className="text-lg justify-center">
+					<FadeIn transitionDuration={600} delay={200}>
+						<div>
+							<h1 className="text-3xl lg:text-6xl textColor">
+								{singlePost.title}
+							</h1>
 						</div>
-						<img
-							src={singlePost.mainImage.asset.url}
-							alt={singlePost.title}
-							className="w-full object-cover rounded-t"
-							style={{ height: "400px" }}
-						/>
-					</header>
-					<div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full text-white">
-						<BlockContent
-							blocks={singlePost.body}
-							projectId={process.env.REACT_APP_SANITY_PROJECT_ID}
-							dataset="production"
-						/>
-					</div>
-				</article>
+					</FadeIn>
+					<FadeIn transitionDuration={600} delay={500}>
+						<div className="py-2 prose lg:prose-xl max-w-full">
+							<BlockContent
+								blocks={singlePost.body}
+								projectId="m7sihnvj"
+								dataset="production"
+								className="text-white"
+							/>
+						</div>
+					</FadeIn>
+				</div>
+			</article>
 			</Suspense>
 		</main>
 	);
